6e7faa43f3c0224c735c5fe389be5e55ad42d74f
Kathleen Pehl <kathleen.pehl@gmail.com>
2021-01-07 18:34:49 -0500

    cache update now works for deleting a book

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -30,7 +30,15 @@ const SavedBooks = () => {
 
     try {
       await deleteBook({
-        variables: {bookId: bookId}
+        variables: {bookId: bookId},
+        update: cache => {
+          const data = cache.readQuery({ query: GET_ME });
+          const userDataCache = data.me;
+          const savedBooksCache = userDataCache.savedBooks;
+          const updatedBookCache = savedBooksCache.filter((book) => book.bookId !== bookId);
+          data.me.savedBooks = updatedBookCache;
+          cache.writeQuery({ query: GET_ME , data: {data: {...data.me.savedBooks}}})
+        }
       });
       // upon success, remove book's id from localStorage
       removeBookId(bookId);
